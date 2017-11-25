1b60d340ba653ebb3856feef57c6dc31abd56c76
raish_varia <raish9396varia@gmail.com>
2017-11-25 15:14:25 +0530

    new plugins added

---
+++ resources/assets/js/components/PieChart.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="graph-container">
+        <canvas id="graph" ref="graph"></canvas>
+    </div>
+</template>
+
+<script>
+    export default {
+
+        props: [
+            'labels',
+            'values',
+            'bgColors',
+            'hoverBgColors'
+        ],
+
+        mounted () {
+            let context = this.$refs.graph.getContext('2d');
+
+            let options = {
+                responsive: true,
+                maintainAspectRatio: false,
+            };
+
+            let data = {
+                labels: this.labels,
+                datasets: [
+                    {
+                        data: this.values,
+                        backgroundColor: this.bgColors,
+                        hoverBackgroundColor: this.hoverBgColors
+                    }]
+            };
+
+            let myPieChart = new Chart(context,{
+                type: 'pie',
+                data: data,
+                options: options
+            });
+        }
+    }
+</script>
+
+<style>
+    .graph-container {
+        height: 300px;
+    }
+</style>
---
+++ resources/assets/plugins/laraspace/laraspace-notifs.js
@@ -0,0 +1,92 @@
+var LaraspaceNotifs = function () {
+
+    var handleNotifs = function(){
+        toastr.options = {
+            "closeButton": true,
+            "debug": false,
+            "positionClass": "toast-top-right",
+            "onclick": null,
+            "showDuration": "1000",
+            "hideDuration": "1000",
+            "timeOut": "5000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+        };
+
+        var notify = $('.laraspace-notify'),
+            type = notify.data('notify'),
+            driver = notify.data('driver'),
+            message = notify.data('message'),title = notify.data('title');
+
+        //Laracasts-Flash returns danger as the error type. However we need it to "error" to work with toastr.
+        if(type == 'danger'){
+            type = 'error';
+        }
+
+        if(!message == '' && driver == 'toastr'){
+            toastr[type](message, title);
+        }else if(!message == ''){
+            notieAlert(type,message);
+        }
+
+    };
+
+    var notieAlert = function(type, message){
+
+        //Change timeout value here.
+        var timeout = 5;
+
+        switch(type) {
+            case 'success':
+                notie.alert({
+                    type : 1,
+                    text : message,
+                    time : timeout
+                })
+                break;
+            case 'warning':
+                notie.alert({
+                    type : 2,
+                    text : message,
+                    time : timeout
+                })
+                break;
+            case 'info':
+                notie.alert({
+                    type : 3,
+                    text : message,
+                    time : timeout
+                })
+                break;
+            case 'error':
+                notie.alert({
+                    type : 4,
+                    text : message,
+                    time : timeout
+                })
+                break;
+            default:
+                notie.alert({
+                    type : 1,
+                    text : message,
+                    time : timeout
+                })
+
+        }
+    };
+
+    return {
+        //main function to initiate the module
+        init: function () {
+            handleNotifs();
+        },
+    };
+
+}();
+
+jQuery(document).ready(function() {
+    LaraspaceNotifs.init();
+});
