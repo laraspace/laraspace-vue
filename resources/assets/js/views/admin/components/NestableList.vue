c58b39a403858cf9ef8d09c43e06c13f11a36871
raish_varia <raish9396varia@gmail.com>
2017-12-05 13:46:57 +0530

    vue js refatcor

--- resources/assets/js/views/admin/components/NestableList.vue
+++ resources/assets/js/views/admin/components/NestableList.vue
@@ -155,64 +155,61 @@
 <script>
     export default {
 
-        methods: {},
         mounted: function () {
-
-            var NestableLists = function () {
-
-                var updateOutput = function (e) {
-                    var list = e.length ? e : $(e.target), output = list.data('output');
-
-                    if (window.JSON) {
-                        output.val(window.JSON.stringify(list.nestable('serialize')));//, null, 2));
-                    } else {
-                        output.val('JSON browser support required for this demo.');
-                    }
-                };
-
-                var handleLists = function () {
-// activate Nestable for list 1
-                    $('#nestable').nestable({group: 1}).on('change', updateOutput);
-
-// activate Nestable for list 2
-                    $('#nestable2').nestable({group: 1}).on('change', updateOutput)
-
-// Draggable
-                    $('#nestable3').nestable();
-                };
-
-                var handleOutput = function () {
-                    updateOutput($('#nestable').data('output', $('#nestable-output')));
-                    updateOutput($('#nestable2').data('output', $('#nestable2-output')));
-                };
-
-                var handleOutputActions = function () {
-                    $('.actions').on('click', function (e) {
-                        var target = $(e.target), action = target.data('action');
-
-                        if (action === 'expand-all') {
-                            $('.dd').nestable('expandAll');
-                        }
-                        if (action === 'collapse-all') {
-                            $('.dd').nestable('collapseAll');
-                        }
-                    });
-                }
-
+            let vm = this;
+            let NestableLists = function () {
                 return {
-//main function to initiate the module
+                    //main function to initiate the module
                     init: function () {
-                        handleLists();
-                        handleOutput();
-                        handleOutputActions();
+                        vm.handleLists();
+                        vm.handleOutput();
+                        vm.handleOutputActions();
                     }
                 };
-
             }();
-
-            jQuery(document).ready(function () {
+            this.$nextTick(() => {
                 NestableLists.init();
             });
         },
+        methods: {
+            updateOutput(e) {
+                let list = e.length ? e : $(e.target), output = list.data('output');
+
+                if (window.JSON) {
+                    output.val(window.JSON.stringify(list.nestable('serialize')));//, null, 2));
+                } else {
+                    output.val('JSON browser support required for this demo.');
+                }
+            },
+
+            handleLists() {
+                // activate Nestable for list 1
+                $('#nestable').nestable({group: 1}).on('change', this.updateOutput);
+
+                // activate Nestable for list 2
+                $('#nestable2').nestable({group: 1}).on('change', this.updateOutput)
+
+                // Draggable
+                $('#nestable3').nestable();
+            },
+
+            handleOutput() {
+                this.updateOutput($('#nestable').data('output', $('#nestable-output')));
+                this.updateOutput($('#nestable2').data('output', $('#nestable2-output')));
+            },
+
+            handleOutputActions() {
+                $('.actions').on('click', function (e) {
+                    let target = $(e.target), action = target.data('action');
+
+                    if (action === 'expand-all') {
+                        $('.dd').nestable('expandAll');
+                    }
+                    if (action === 'collapse-all') {
+                        $('.dd').nestable('collapseAll');
+                    }
+                });
+            }
+        }
     }
-    </script>
+</script>
