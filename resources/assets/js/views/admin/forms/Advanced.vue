1811d7edd6ca02ecd7e676c17f605b2344c9ccda
yogesh_gohil <yogeshgohil1611@gmail.com>
2018-02-01 18:01:50 +0530

    vue-color Added

--- resources/assets/js/views/admin/forms/Advanced.vue
+++ resources/assets/js/views/admin/forms/Advanced.vue
@@ -480,12 +480,97 @@
         </div>
       </div>
     </div>
+    <div class="card">
+      <div class="card-header">
+        <h6>Vue Color Picker <a
+          class="source-link"
+          href="http://xiaokaike.github.io/vue-color/"
+          target="_blank">source</a></h6>
+      </div>
+      <div
+        class="card-body"
+        :style="{ 'background': colors.hex }">
+        <div class="row">
+          <div class="col-xl-4 mb-4">
+            <p>Photoshop Picker</p>
+            <photoshop-picker v-model="colors" />
+          </div>
+          <div class="col-xl-4 mb-4">
+            <p>Slider Picker</p>
+            <slider-picker v-model="colors" />
+            <br>
+            <div class="row">
+              <div class="sketch">
+                <sketch-picker v-model="colors" />
+              </div>
+              <div class="chrome">
+                <chrome-picker v-model="colors" />
+              </div>
+            </div>
+          </div>
+          <div class="col-xl-4 mb-4">
+            <material-picker v-model="colors" /><br>
+            <compact-picker v-model="colors" /><br>
+            <swatches-picker v-model="colors" />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script type="text/babel">
+import { Photoshop, Material, Compact, Swatches, Slider, Chrome, Sketch } from 'vue-color'
+
+var defaultProps = {
+  hex: '#194d33',
+  hsl: {
+    h: 150,
+    s: 0.5,
+    l: 0.2,
+    a: 1
+  },
+  hsv: {
+    h: 150,
+    s: 0.66,
+    v: 0.30,
+    a: 1
+  },
+  rgba: {
+    r: 25,
+    g: 77,
+    b: 51,
+    a: 1
+  },
+  a: 1
+}
+
 export default {
+  components: {
+    'material-picker': Material,
+    'compact-picker': Compact,
+    'swatches-picker': Swatches,
+    'slider-picker': Slider,
+    'sketch-picker': Sketch,
+    'chrome-picker': Chrome,
+    'photoshop-picker': Photoshop
+  },
+  data () {
+    return {
+      colors: defaultProps
+    }
+  },
   mounted () {
     Plugin.initPlugins(['Select2', 'TimePickers', 'DatePicker', 'MultiSelect', 'BootstrapSelect', 'SwitchToggles'])
   }
 }
 </script>
+<style>
+.sketch{
+  padding-right: 15px;
+  padding-left: 15px;
+}
+.vc-material{
+    width: 120px;
+    height: 120px;
+}
+</style>
--- resources/assets/js/views/layouts/partials/TheSiteSidebar.vue
+++ resources/assets/js/views/layouts/partials/TheSiteSidebar.vue
@@ -91,9 +91,6 @@
               to="/admin/components/calendar"
               tag="li"><a>Calendar</a></router-link>
             <router-link
-              to="/admin/components/mail-box"
-              tag="li"><a>MailBox</a></router-link>
-            <router-link
               to="/admin/components/datatables"
               tag="li"><a>Jquery Datatables</a></router-link>
             <router-link
