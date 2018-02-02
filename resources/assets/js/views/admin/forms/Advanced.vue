0202857e664da901ad0bc5eb2f8c083d2cc56e3d
yogesh_gohil <yogeshgohil1611@gmail.com>
2018-02-02 17:56:13 +0530

    Vue-Multiselect Added

--- resources/assets/js/views/admin/forms/Advanced.vue
+++ resources/assets/js/views/admin/forms/Advanced.vue
@@ -480,10 +480,91 @@
         </div>
       </div>
     </div>
+    <div class="card">
+      <div class="card-header">
+        <h6>Multi Select <a
+          class="source-link"
+          href="https://vue-multiselect.js.org/"
+          target="_blank">source</a></h6>
+      </div>
+      <div class="card-body">
+        <div class="row">
+          <div class="col-xl-4 mb-4">
+            <h5 class="section-semi-title">SINGLE SELECT</h5>
+            <multiselect
+              v-model="value"
+              :options="SingleOptions"
+              :searchable="true"
+              :close-on-select="true"
+              :show-labels="true"
+              placeholder="Pick a value"
+            />
+          </div>
+          <div class="col-xl-4 mb-4">
+            <h5 class="section-semi-title">WITH GROUPS</h5>
+            <multiselect
+              v-model="GroupValue"
+              :options="GroupOptions"
+              :multiple="true"
+              group-values="libs"
+              group-label="language"
+              placeholder="Type to search"
+              track-by="name"
+              label="name">
+              <span slot="noResult">Oops! No elements found. Consider changing the search query.</span>
+            </multiselect>
+          </div>
+          <div class="col-xl-4 mb-4">
+            <h5 class="section-semi-title">MULTI SELECT</h5>
+            <multiselect
+              v-model="selected"
+              :options="MultiOptions"
+              :multiple="true"
+              :custom-label="customLabel"/>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script type="text/babel">
+import Multiselect from 'vue-multiselect'
+import 'vue-multiselect/dist/vue-multiselect.min.css'
 export default {
+  components: { Multiselect },
+  data () {
+    return {
+      selected: null,
+      value: '',
+      GroupValue: '',
+      SingleOptions: ['Select option', 'options', 'selected', 'mulitple', 'label', 'searchable', 'clearOnSelect', 'hideSelected', 'maxHeight', 'allowEmpty', 'showLabels', 'onChange', 'touched'],
+      MultiOptions: ['list', 'of', 'options'],
+      GroupOptions: [
+        {
+          language: 'Javascript',
+          libs: [
+            { name: 'Vue.js', category: 'Front-end' },
+            { name: 'Adonis', category: 'Backend' }
+          ]
+        },
+        {
+          language: 'Ruby',
+          libs: [
+            { name: 'Rails', category: 'Backend' },
+            { name: 'Sinatra', category: 'Backend' }
+          ]
+        },
+        {
+          language: 'Other',
+          libs: [
+            { name: 'Laravel', category: 'Backend' },
+            { name: 'Phoenix', category: 'Backend' }
+          ]
+        }
+      ],
+      value: []
+    }
+  },
   mounted () {
     Plugin.initPlugins(['Select2', 'TimePickers', 'DatePicker', 'MultiSelect', 'BootstrapSelect', 'SwitchToggles'])
   }
