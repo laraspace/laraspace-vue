bcf7311fdc1aa3e84b9a67fdf04d1cd3e3fb490b
yogesh_gohil <yogeshgohil1611@gmail.com>
2018-02-10 15:21:07 +0530

    all tabs are working fine

--- resources/assets/js/views/admin/components/VueTabs.vue
+++ resources/assets/js/views/admin/components/VueTabs.vue
@@ -10,55 +10,286 @@
     </div>
     <div class="card">
       <div class="card-header">
-        <h6>Vue Tabs <a
-          class="source-link"
-          href="http://vue-tabs-component.spatie.be/"
-          target="_blank">source</a></h6>
+        <h6>Tabs Component</h6>
       </div>
       <div class="card-body">
-        <div class="col-lg-6 mb-4">
-          <div>
+        <div class="row">
+          <div class="default col-lg-6 mb-4">
             <tabs>
               <tab name="First tab">
-                <h2 class="page-subtitle">First tab</h2>
+                <h5 class="page-subtitle">First tab</h5>
                 This is the content of the first tab
                 <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio quibusdam maxime amet tenetur quasi nobis maiores dolores voluptates, soluta quae alias ratione iure. Consequuntur asperiores at placeat officia tempora quidem.
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                 </p>
               </tab>
               <tab name="Second tab">
-                <h2 class="page-subtitle">Second tab</h2>
+                <h5 class="page-subtitle">Second tab</h5>
                 This is the content of the second tab
                 <p>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui hic corporis aspernatur non, odit sed libero harum quos nemo eaque adipisci nihil placeat ipsum, esse suscipit doloribus cum? Nihil, pariatur.
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                 </p>
               </tab>
               <tab
                 name="Disabled tab"
                 :is-disabled="true">
-                <h2 class="page-subtitle">Disabled tab</h2>
+                <h5 class="page-subtitle">Disabled tab</h5>
                 This content will be unavailable while :is-disabled prop set to true
                 <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti placeat eius tempore facere, ipsum nulla molestias dolorum deserunt quasi maxime perspiciatis impedit itaque libero a sed eveniet nostrum quidem vel!
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                 </p>
               </tab>
               <tab
                 id="oh-hi-mark"
                 name="Custom fragment">
-                <h2 class="page-subtitle">Custom Fragment</h2>
+                <h5 class="page-subtitle">Custom Fragment</h5>
                 The fragment that is appended to the url can be customized
                 <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque officia esse necessitatibus quo temporibus fugiat placeat qui voluptas? Perspiciatis officiis cumque adipisci tempora eligendi est tenetur nam modi aut vero.
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 </p>
               </tab>
               <tab
                 prefix="<span class='prefix'>→</span> "
                 name="Prefix and suffix"
                 suffix=" <span class='suffix'>4</span>">
-                <h2 class="page-subtitle">Prefix and Suffix</h2>
+                <h5 class="page-subtitle">Prefix and Suffix</h5>
                 A prefix and a suffix can be added
                 <p>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime in at nihil laborum labore dolore, tempora deserunt est recusandae quis. Ratione sint ducimus animi! Provident architecto mollitia libero nobis nostrum?
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                </p>
+              </tab>
+            </tabs>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-header">
+        <h6>Tabs Defaults</h6>
+      </div>
+      <div class="card-body">
+        <div class="row">
+          <div class="basic col-lg-6 mb-4">
+            <h5 class="section-semi-title">Basic</h5>
+            <tabs>
+              <tab
+                name="Home"
+                id="basic-home">
+                This is the content of the first tab
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                </p>
+              </tab>
+              <tab
+                name="Profile"
+                id="basic-profile">
+                This is the content of the second tab
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                </p>
+              </tab>
+              <tab
+                id="basic-message"
+                name="Message">
+                The fragment that is appended to the url can be customized
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+              </tab>
+              <tab
+                name="Setting"
+                id="basic-setting">
+                A prefix and a suffix can be added
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                </p>
+              </tab>
+            </tabs>
+          </div>
+          <div class="basic-primary col-lg-6 mb-4">
+            <h5 class="section-semi-title">basic primary color</h5>
+            <tabs>
+              <tab
+                name="Home"
+                id="basic-primary-home">
+                This is the content of the first tab
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                </p>
+              </tab>
+              <tab
+                name="Profile"
+                id="basic-primary-profile">
+                This is the content of the second tab
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              </tab>
+              <tab
+                id="basic-primary-message"
+                name="Message">
+                The fragment that is appended to the url can be customized
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+              </tab>
+              <tab
+                name="Setting"
+                id="basic-primary-setting">
+                A prefix and a suffix can be added
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                </p>
+              </tab>
+            </tabs>
+          </div>
+        </div>
+        <div class="row">
+          <div class="basic-vertical col-lg-6 mb-4">
+            <h5 class="section-semi-title">VERTICAL</h5>
+            <tabs>
+              <tab
+                name="Home"
+                id="basic-vertical-home">
+                This is the content of the first tab
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                </p>
+              </tab>
+              <tab
+                name="Profile"
+                id="basic-vertical-profile">
+                This is the content of the second tab
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                </p>
+              </tab>
+              <tab
+                id="basic-vertical-message"
+                name="Message">
+                The fragment that is appended to the url can be customized
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+              </tab>
+              <tab
+                id="basic-vertical-setting"
+                name="Setting">
+                A prefix and a suffix can be added
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                </p>
+              </tab>
+            </tabs>
+          </div>
+          <div class="basic-vertical-primary col-lg-6 mb-4">
+            <h5 class="section-semi-title">vertical primary color</h5>
+            <tabs>
+              <tab
+                id="vertical-primary-home"
+                name="Home">
+                This is the content of the first tab
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                </p>
+              </tab>
+              <tab
+                id="vertical-primary-Profile"
+                name="Profile">
+                This is the content of the second tab
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                </p>
+              </tab>
+              <tab
+                id="vertical-primary-message"
+                name="Message">
+                The fragment that is appended to the url can be customized
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+              </tab>
+              <tab
+                id="vertical-primary-setting"
+                name="Fourth">
+                A prefix and a suffix can be added
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                </p>
+              </tab>
+            </tabs>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-header">
+        <h6>Tabs Simple</h6>
+      </div>
+      <div class="card-body">
+        <div class="row">
+          <div class="simple col-lg-6 mb-4">
+            <h5 class="section-semi-title">BASIC</h5>
+            <tabs>
+              <tab
+                id="simple-home"
+                name="Home">
+                This is the content of the first tab
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                </p>
+              </tab>
+              <tab
+                id="simple-profile"
+                name="Profile">
+                This is the content of the second tab
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui hic corporis aspernatur non.
+                </p>
+              </tab>
+              <tab
+                id="simple-message"
+                name="Message">
+                The fragment that is appended to the url can be customized
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+              </tab>
+              <tab
+                id="simple-setting"
+                name="Setting">
+                A prefix and a suffix can be added
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                </p>
+              </tab>
+            </tabs>
+          </div>
+          <div class="vertical col-lg-6 mb-4">
+            <h5 class="section-semi-title">VERTICAL</h5>
+            <tabs>
+              <tab name="Home">
+                This is the content of the first tab
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                </p>
+              </tab>
+              <tab name="Profile">
+                This is the content of the second tab
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                </p>
+              </tab>
+              <tab
+                id="hello-mark"
+                name="Message">
+                The fragment that is appended to the url can be customized
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+              </tab>
+              <tab name="Setting">
+                A prefix and a suffix can be added
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                 </p>
               </tab>
             </tabs>
@@ -71,47 +302,10 @@
 <script>
 import {Tabs, Tab} from 'vue-tabs-component'
 import 'vue-tabs-component/docs/resources/tabs-component.css'
-export default{
+export default {
   components: {
     'tabs': Tabs,
     'tab': Tab
   }
 }
 </script>
-<style>
-
-.suffix{
-  background-color: #c03;
-  color: #fff;
-  margin-left: .35em;
-}
-.prefix {
-    background-color: #d1e8eb;
-    color: #0c5174;
-    margin-right: .35em;
-}
-.prefix , .suffix{
-    align-items: center;
-    border-radius: 1.25rem;
-    display: flex;
-    font-size: .75rem;
-    flex-shrink: 0;
-    height: 1.25rem;
-    justify-content: center;
-    line-height: 1.25rem;
-    min-width: 1.25rem;
-    padding: 0 .1em;
-}
-.page-subtitle {
-    font-size: 1.25rem;
-    margin-bottom: 1em;
-    padding-top: .25em;
-}
-@media (min-width: 700px){
-.suffix {
-    position: absolute;
-    right: -.725em;
-    top: -.725em;
-  }
-}
-</style>
