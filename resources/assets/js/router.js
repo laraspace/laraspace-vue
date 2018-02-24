ba665958e0b903ee2f04f824669fd87dda042cfb
yogesh_gohil <yogeshgohil1611@gmail.com>
2018-02-24 15:57:04 +0530

    Vue-carousel added

--- resources/assets/js/router.js
+++ resources/assets/js/router.js
@@ -38,6 +38,7 @@ import ImageZoom from './views/admin/components/ImageZoom.vue'
 import Calendar from './views/admin/components/Calendar.vue'
 import NestableTree from './views/admin/components/NestableTree.vue'
 import NestableList from './views/admin/components/NestableList.vue'
+import VueCarousel from './views/admin/components/VueCarousel.vue'
 // 1. Ratings
 import BarRating from './views/admin/components/ratings/BarRating.vue'
 import StarRating from './views/admin/components/ratings/StarRating.vue'
@@ -253,6 +254,10 @@ const routes = [
       {
         path: 'components/sweet-modals',
         component: SweetModals
+      },
+      {
+        path: 'components/vue-carousel',
+        component: VueCarousel
       },
       // Rating
       {
