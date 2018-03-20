97d031b7802059729494e9025d211bda7a916be3
yogesh_gohil <yogeshgohil1611@gmail.com>
2018-03-20 16:55:20 +0530

    All Jquery Related Component is Removed from this branch

--- resources/assets/js/helpers/plugin.js
+++ resources/assets/js/helpers/plugin.js
@@ -1,68 +1,20 @@
 export default {
-  EasyPieChart () {
-    var elems = $('.easy-pie-chart')
-    elems.each(function (index, element) {
-      var color = $(this).data('color') ? $(this).data('color') : '#ffde00'
+  // EasyPieChart () {
+  //   var elems = $('.easy-pie-chart')
+  //   elems.each(function (index, element) {
+  //     var color = $(this).data('color') ? $(this).data('color') : '#ffde00'
 
-      let myBarChart = new EasyPieChart(element, {
-        scaleColor: false,
-        barColor: color,
-        trackColor: '#f8f8f8',
-        size: 80,
-        onStep: function (from, to, percent) {
-          $(this.el).find('.percent').text(Math.round(percent))
-        }
-      })
-    })
-  },
-
-  MetisMenu () {
-    $('#menu').metisMenu()
-  },
-
-  Select2 () {
-    jquery('.ls-select2').select2()
-  },
-
-  SwitchToggles () {
-    var elems = $('.ls-switch')
-
-    elems.each(function (index, element) {
-      var color = $(this).data('color') ? $(this).data('color') : '#ffde00'
-      var size = $(this).data('size') ? $(this).data('size') : 'default'
-
-      var switchery = new Switchery(this, {
-        color: color,
-        size: size
-      })
-    })
-  },
-
-  TimePickers () {
-    window.$('.ls-clockpicker').clockpicker({
-      donetext: 'Done'
-    })
-    var elems = $('.ls-timepicker')
-    elems.each(function (index, element) {
-      var timeFormat = $(this).data('format') ? $(this).data('format') : 'g:ia'
-      var showDuration = $(this).data('duration') ? $(this).data('duration') : false
-
-      window.jquery(this).timepicker({
-
-        timeFormat: timeFormat,
-        showDuration: showDuration,
-        minTime: '2:00pm',
-        maxTime: '11:30pm'
-      })
-    })
-  },
-  MultiSelect () {
-    $('.ls-multi-select').multiSelect()
-  },
-
-  DatePicker () {
-    jquery('.ls-datepicker').datepicker()
-  },
+  //     let myBarChart = new EasyPieChart(element, {
+  //       scaleColor: false,
+  //       barColor: color,
+  //       trackColor: '#f8f8f8',
+  //       size: 80,
+  //       onStep: function (from, to, percent) {
+  //         $(this.el).find('.percent').text(Math.round(percent))
+  //       }
+  //     })
+  //   })
+  // },
 
   Editors () {
     $('.ls-summernote').summernote()
@@ -84,18 +36,5 @@ export default {
   isFunction (functionToCheck) {
     var getType = {}
     return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]'
-  },
-  CustomScrollBars () {
-    // setTimeout(function (){
-    //     $('.scroll-pane').mCustomScrollbar({theme:'minimal-dark'})
-    // },1000)
-    jquery('.scroll-pane').mCustomScrollbar({ theme: 'minimal-dark' })
-  },
-
-  DataTables () {
-    jquery('#default-datatable').DataTable()
-    jquery('#responsive-datatable').DataTable({
-      responsive: true
-    })
   }
 }
