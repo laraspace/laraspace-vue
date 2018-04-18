74fd71760b84dae4a5a7fd217c0c7341b08a3ae3
yogesh_gohil <yogeshgohil1611@gmail.com>
2018-04-18 15:41:24 +0530

    Vue-color Added in AdvancedForm

--- resources/assets/js/views/admin/forms/Advanced.vue
+++ resources/assets/js/views/admin/forms/Advanced.vue
@@ -101,6 +101,42 @@
         </div>
       </div>
     </div>
+    <div class="card">
+      <div class="card-header">
+        <h6>Vue Color Picker <a
+          class="source-link"
+          href="http://xiaokaike.github.io/vue-color/"
+          target="_blank">source</a></h6>
+      </div>
+      <div
+        class="card-body"
+        :style="{ 'background': colors.hex }">
+        <div class="row">
+          <div class="col-xl-4 mb-4">
+            <p>Photoshop Picker</p>
+            <photoshop-picker v-model="colors" />
+          </div>
+          <div class="col-xl-4 mb-4">
+            <p>Slider Picker</p>
+            <slider-picker v-model="colors" />
+            <br>
+            <div class="row">
+              <div class="sketch">
+                <sketch-picker v-model="colors" />
+              </div>
+              <div class="chrome">
+                <chrome-picker v-model="colors" />
+              </div>
+            </div>
+          </div>
+          <div class="col-xl-4 mb-4">
+            <material-picker v-model="colors" /><br>
+            <compact-picker v-model="colors" /><br>
+            <swatches-picker v-model="colors" />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script type="text/babel">
@@ -108,16 +144,48 @@ import MaskedInput from 'vue-text-mask'
 import Multiselect from 'vue-multiselect'
 import 'vue-multiselect/dist/vue-multiselect.min.css'
 import createAutoCorrectedDatePipe from 'text-mask-addons/dist/createAutoCorrectedDatePipe'
+import { Photoshop, Material, Compact, Swatches, Slider, Chrome, Sketch } from 'vue-color'
 const autoCorrectedDatePipe = createAutoCorrectedDatePipe('mm/dd/yyyy HH:MM')
+let defaultProps = {
+  hex: '#194d33',
+  hsl: {
+    h: 150,
+    s: 0.5,
+    l: 0.2,
+    a: 1
+  },
+  hsv: {
+    h: 150,
+    s: 0.66,
+    v: 0.30,
+    a: 1
+  },
+  rgba: {
+    r: 25,
+    g: 77,
+    b: 51,
+    a: 1
+  },
+  a: 1
+}
+
 export default {
   name: 'name',
   components: {
     'masked-input': MaskedInput,
-     Multiselect,
+    'multiselect': Multiselect,
+    'material-picker': Material,
+    'compact-picker': Compact,
+    'swatches-picker': Swatches,
+    'slider-picker': Slider,
+    'sketch-picker': Sketch,
+    'chrome-picker': Chrome,
+    'photoshop-picker': Photoshop
     
   },
   data () {
     return {
+      colors: defaultProps,
       phone: '',
       date: '',
       dateAuto: '',
@@ -158,3 +226,13 @@ export default {
   }
 }
 </script>
+<style>
+.sketch{
+  padding-right: 15px;
+  padding-left: 15px;
+}
+.vc-material{
+    width: 120px;
+    height: 120px;
+}
+</style>
