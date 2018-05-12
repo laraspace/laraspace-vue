f0209be5e44c120b3f51103cd78b216c04c6a99e
yogesh_gohil <yogeshgohil1611@gmail.com>
2018-05-12 11:22:46 +0530

    esling formating

--- resources/assets/js/components/advanced-elements/MultiSelect.vue
+++ resources/assets/js/components/advanced-elements/MultiSelect.vue
@@ -1,38 +1,40 @@
 <template>
-      <div class="row">
-          <div class="col-xl-4 mb-4">
-            <h5 class="section-semi-title">SINGLE SELECT</h5>
-            <multiselect
-              v-model="value"
-              :options="SingleOptions"
-              :searchable="true"
-              :close-on-select="true"
-              :show-labels="true"
-              placeholder="Pick a value"
-            />
-          </div>
-          <div class="col-xl-4 mb-4">
-            <h5 class="section-semi-title">WITH GROUPS</h5>
-            <multiselect
-              v-model="GroupValue"
-              :options="GroupOptions"
-              :multiple="true"
-              group-values="city"
-              group-label="timezone"
-              placeholder="Type to search"
-              track-by="name"
-              label="name">
-              <span slot="noResult">Oops! No elements found. Consider changing the search query.</span>
-            </multiselect>
-          </div>
-          <div class="col-xl-4 mb-4">
-            <h5 class="section-semi-title">MULTI SELECT</h5>
-            <multiselect
-              v-model="selected"
-              :options="MultiOptions"
-              :multiple="true"/>
-          </div>
-        </div>
+  <div class="row">
+    <div class="col-xl-4 mb-4">
+      <h5 class="section-semi-title">SINGLE SELECT</h5>
+      <multiselect
+        v-model="value"
+        :options="SingleOptions"
+        :searchable="true"
+        :close-on-select="true"
+        :show-labels="true"
+        placeholder="Pick a value"
+      />
+    </div>
+    <div class="col-xl-4 mb-4">
+      <h5 class="section-semi-title">WITH GROUPS</h5>
+      <multiselect
+        v-model="GroupValue"
+        :options="GroupOptions"
+        :multiple="true"
+        group-values="city"
+        group-label="timezone"
+        placeholder="Type to search"
+        track-by="name"
+        label="name"
+      >
+        <span slot="noResult">Oops! No elements found. Consider changing the search query.</span>
+      </multiselect>
+    </div>
+    <div class="col-xl-4 mb-4">
+      <h5 class="section-semi-title">MULTI SELECT</h5>
+      <multiselect
+        v-model="selected"
+        :options="MultiOptions"
+        :multiple="true"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -46,86 +48,86 @@ export default {
       value: '',
       GroupValue: '',
       SingleOptions: ['Alaska', 'Hawaii', 'California', 'Nevada', 'Oregon', 'Washington'],
-      MultiOptions:  ['Alaska', 'Hawaii', 'California', 'Nevada', 'Oregon', 'Washington'],
+      MultiOptions: ['Alaska', 'Hawaii', 'California', 'Nevada', 'Oregon', 'Washington'],
       GroupOptions: [
         {
           timezone: 'Alaskan/Hawaiian Time Zone',
           city: [
-            { name: 'Alaska'},
-            { name: 'Hawaii'}
+            {name: 'Alaska'},
+            {name: 'Hawaii'}
           ]
         },
         {
           timezone: 'Pacific Time Zone',
           city: [
-            { name: 'California'},
-            { name: 'Nevada'},
-            { name: 'Oregon'},
-            { name: 'Washington'},
+            {name: 'California'},
+            {name: 'Nevada'},
+            {name: 'Oregon'},
+            {name: 'Washington'}
           ]
         },
         {
           timezone: 'Mountain Time Zone',
           city: [
-            { name: 'Arizona'},
-            { name: 'Colorado'},
-            { name: 'Idaho'},
-            { name: 'Montana'},
-            { name: 'Nebraska'},
-            { name: 'New Mexico'},
-            { name: 'North Dakota'},
-            { name: 'Utah'},
-            { name: 'Wyoming'},
+            {name: 'Arizona'},
+            {name: 'Colorado'},
+            {name: 'Idaho'},
+            {name: 'Montana'},
+            {name: 'Nebraska'},
+            {name: 'New Mexico'},
+            {name: 'North Dakota'},
+            {name: 'Utah'},
+            {name: 'Wyoming'}
           ]
         },
         {
           timezone: 'Central Time Zone',
           city: [
-            { name: 'Alabama'},
-            { name: 'Arkansas'},
-            { name: 'Illinois'},
-            { name: 'Iowa'},
-            { name: 'Kansas'},
-            { name: 'Kentucky'},
-            { name: 'Louisiana'},
-            { name: 'Minnesota'},
-            { name: 'Mississippi'},
-            { name: 'Missouri'},
-            { name: 'Oklahoma'},
-            { name: 'South Dakota'},
-            { name: 'Texas'},
-            { name: 'Tennessee'},
-            { name: 'Wisconsin'},
+            {name: 'Alabama'},
+            {name: 'Arkansas'},
+            {name: 'Illinois'},
+            {name: 'Iowa'},
+            {name: 'Kansas'},
+            {name: 'Kentucky'},
+            {name: 'Louisiana'},
+            {name: 'Minnesota'},
+            {name: 'Mississippi'},
+            {name: 'Missouri'},
+            {name: 'Oklahoma'},
+            {name: 'South Dakota'},
+            {name: 'Texas'},
+            {name: 'Tennessee'},
+            {name: 'Wisconsin'}
           ]
         },
         {
           timezone: 'Eastern Time Zone',
           city: [
-            { name: 'Connecticut'},
-            { name: 'Delaware'},
-            { name: 'Florida'},
-            { name: 'Georgia'},
-            { name: 'Indiana'},
-            { name: 'Maine'},
-            { name: 'Maryland'},
-            { name: 'Massachusetts'},
-            { name: 'Michigan'},
-            { name: 'New Hampshire'},
-            { name: 'New Jersey'},
-            { name: 'New York'},
-            { name: 'North Carolina'},
-            { name: 'Ohio'},
-            { name: 'Pennsylvania'},
-            { name: 'Rhode Island'},
-            { name: 'South Carolina'},
-            { name: 'Vermont'},
-            { name: 'Virginia'},
-            { name: 'West Virginia'},
+            {name: 'Connecticut'},
+            {name: 'Delaware'},
+            {name: 'Florida'},
+            {name: 'Georgia'},
+            {name: 'Indiana'},
+            {name: 'Maine'},
+            {name: 'Maryland'},
+            {name: 'Massachusetts'},
+            {name: 'Michigan'},
+            {name: 'New Hampshire'},
+            {name: 'New Jersey'},
+            {name: 'New York'},
+            {name: 'North Carolina'},
+            {name: 'Ohio'},
+            {name: 'Pennsylvania'},
+            {name: 'Rhode Island'},
+            {name: 'South Carolina'},
+            {name: 'Vermont'},
+            {name: 'Virginia'},
+            {name: 'West Virginia'}
           ]
         }
-      ],
-      value: []
+      ]
+      // value: []
     }
-  },
+  }
 }
 </script>
