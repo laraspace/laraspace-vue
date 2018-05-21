520c93742780c0afaac7436880c585d75146a344
Mohit Panjwani <mohit.panjvani@gmail.com>
2018-05-21 12:21:42 +0530

    mailbox and async await changes

--- resources/assets/js/views/admin/apps/mailbox/MailboxModal.vue
+++ resources/assets/js/views/admin/apps/mailbox/MailboxModal.vue
@@ -1,102 +1,39 @@
 <template>
-  <div v-click-outside="closeModal" :class="['mailbox-modal', { 'is-visible': isVisible }]" >
-    <div class="sidepanel-header">
-      <h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
-      <div class="mail-action">
-        <a
-          data-toggle="dropdown"
-          aria-haspopup="true"
-          aria-expanded="false"
-        >
-          <i class="icon-fa icon-fa-ellipsis-h"/>
-        </a>
-        <div class="dropdown-menu mail-menu">
-          <ul>
-            <li>Archive</li>
-            <li>Spam</li>
-            <li>Delete</li>
-            <li>Print</li>
-          </ul>
-        </div>
-        <a href="#"><i class="icon-fa icon-fa-chevron-left"/></a>
-        <a href="#"><i class="icon-fa icon-fa-chevron-right"/></a>
-      </div>
-      <button class="sidepanel-close" @click="closeModal">
-        <i class="icon-fa icon-fa-close"/>
-      </button>
-    </div>
-    <div class="sidepanel-body">
-      <div class="message-heading">
-        <div class="message-heading-main">
-          <a>
-            <img
-              src="/assets/img/avatars/avatar.png"
-              alt="avtar"
-              class="avtar-img"
-            >
-          </a>
-          <span class="name">John Deo</span>
-          <div class="email">
-            <span>
-              <a href="#">
-                johndeo@gmail.com
-              </a>
-              to
-              <a href="#">
-                me
-              </a>
-            </span>
-          </div>
-          <div class="message-heading-right">
-            <span>3 minute ago</span>
-            <button class="btn btn-light">
-              <i class="icon-fa icon-fa-star-o"/>
-            </button>
-            <button class="btn btn-light">
-              <i class="icon-fa icon-fa-mail-reply"/>
-            </button>
-          </div>
-        </div>
-      </div>
-      <div class="message-content">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur recusandae quia esse inventore animi eligendi optio temporibus veniam asperiores? Dolorem non quos error, aut est libero nisi veniam provident ratione.
-      </div>
-      <div class="message-attachment">
-        <p>
-          <i class="icon-fa icon-fa-paperclip"/>
-          Attachments |
-          <a href="#">Download All</a>
-        </p>
-        <ul>
-          <li>
-            <span class="name">abc.mp4</span>
-            <span class="size">(2.50mb)</span>
-            <button class="btn btn-light">
-              <i class="icon-fa icon-fa-cloud-download"/>
-            </button>
+  <div :class="['mailbox-modal', { 'is-visible': isVisible }]" >
+    <div class="mail-header">
+      <div class="mail-actions">
+        <ul class="mail-action-list">
+          <li class="action">
+            <a href="#" ><i class="icon-fa icon-fa-arrow-left"></i></a>
           </li>
-          <li>
-            <span class="name">image.jpeg</span>
-            <span class="size">(1.50mb)</span>
-            <button class="btn btn-light">
-              <i class="icon-fa icon-fa-cloud-download"/>
-            </button>
+          <li class="action">
+            <a href="#" ><i class="icon-fa icon-fa-exclamation"></i></a>
+          </li>
+          <li class="action">
+            <a href="#" ><i class="icon-fa icon-fa-trash"></i></a>
+          </li>
+          <li class="action">
+            <a href="#" ><i class="icon-fa icon-fa-envelope-open icon-fa-fw"></i></a>
           </li>
         </ul>
       </div>
-      <div class="all-mail">
+    </div>
+    <div class="mail-body">
+      <h3 class="mail-subject">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h3>
+
+      <div class="message">
         <div class="message-heading">
-          <div class="message-heading-main">
-            <a>
+          <div class="message-from">
+            <a class="avatar">
               <img
                 src="/assets/img/avatars/avatar.png"
-                alt="avtar"
-                class="avtar-img"
+                alt="avatar"
+                class="avatar-img"
               >
             </a>
-            <span class="name">John Deo</span>
-            <div class="email">
-              <span>
+            <div class="from-text">
+              <div class="name">John Deo</div>
+              <div class="email">
                 <a href="#">
                   johndeo@gmail.com
                 </a>
@@ -104,29 +41,94 @@
                 <a href="#">
                   me
                 </a>
-              </span>
+              </div>
             </div>
-            <div class="message-heading-right">
-              <span>3 minute ago</span>
-              <button class="btn btn-light">
-                <i class="icon-fa icon-fa-star-o"/>
+          </div>
+
+          <div class="message-heading-right">
+            <span>3 minute ago</span>
+            <button class="btn btn-light btn-icon btn-sm">
+              <i class="icon-fa icon-fa-star-o"/>
+            </button>
+            <button class="btn btn-light btn-icon btn-sm">
+              <i class="icon-fa icon-fa-mail-reply"/>
+            </button>
+          </div>
+        </div>
+        <div class="message-content">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur recusandae quia esse inventore animi eligendi optio temporibus veniam asperiores? Dolorem non quos error, aut est libero nisi veniam provident ratione.
+        </div>
+        <div class="message-attachment">
+          <p>
+            <i class="icon-fa icon-fa-paperclip"/>
+            Attachments |
+            <a href="#">Download All</a>
+          </p>
+          <ul>
+            <li>
+              <span class="name">abc.mp4</span>
+              <span class="size">(2.50mb)</span>
+              <button class="btn btn-light btn-icon btn-sm">
+                <i class="icon-fa icon-fa-cloud-download"/>
               </button>
-              <button class="btn btn-light">
-                <i class="icon-fa icon-fa-mail-reply"/>
+            </li>
+            <li>
+              <span class="name">image.jpeg</span>
+              <span class="size">(1.50mb)</span>
+              <button class="btn btn-light btn-icon btn-sm">
+                <i class="icon-fa icon-fa-cloud-download"/>
               </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="message">
+        <div class="message-heading">
+          <div class="message-from">
+            <a class="avatar">
+              <img
+                src="/assets/img/avatars/avatar.png"
+                alt="avatar"
+                class="avatar-img"
+              >
+            </a>
+            <div class="from-text">
+              <span class="name">John Deo</span>
+              <div class="email">
+                <a href="#">
+                  johndeo@gmail.com
+                </a>
+                to
+                <a href="#">
+                  me
+                </a>
+              </div>
             </div>
+          </div>
+
+          <div class="message-heading-right">
+            <span>3 minute ago</span>
+            <button class="btn btn-light btn-icon btn-sm">
+              <i class="icon-fa icon-fa-star-o"/>
+            </button>
+            <button class="btn btn-light btn-icon btn-sm">
+              <i class="icon-fa icon-fa-mail-reply"/>
+            </button>
           </div>
         </div>
         <div class="message-content">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur recusandae quia esse inventore animi eligendi optio temporibus veniam asperiores.
         </div>
+      </div>
+      <div class="message">
         <div class="message-heading">
-          <div class="message-heading-main">
-            <a>
+          <div class="message-from">
+            <a class="avatar">
               <img
                 src="/assets/img/avatars/avatar.png"
-                alt="avtar"
-                class="avtar-img"
+                alt="avatar"
+                class="avatar-img"
               >
             </a>
             <span class="name">John Deo</span>
@@ -141,15 +143,16 @@
                 </a>
               </span>
             </div>
-            <div class="message-heading-right">
-              <span>3 minute ago</span>
-              <button class="btn btn-light">
-                <i class="icon-fa icon-fa-star-o"/>
-              </button>
-              <button class="btn btn-light">
-                <i class="icon-fa icon-fa-mail-reply"/>
-              </button>
-            </div>
+          </div>
+
+          <div class="message-heading-right">
+            <span>3 minute ago</span>
+            <button class="btn btn-light btn-icon btn-sm">
+              <i class="icon-fa icon-fa-star-o"/>
+            </button>
+            <button class="btn btn-light btn-icon btn-sm">
+              <i class="icon-fa icon-fa-mail-reply"/>
+            </button>
           </div>
         </div>
         <div class="message-content">
