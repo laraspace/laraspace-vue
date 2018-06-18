718cd12874fcc22c1a1a71172c12de5084f83c69
Mohit Panjwani <mohit.panjvani@gmail.com>
2018-06-18 17:46:23 +0530

    remove unused styles and files

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,5 @@
 let mix = require('laravel-mix')
 
-/*
- |--------------------------------------------------------------------------
- | Admin
- |--------------------------------------------------------------------------
- */
-
 mix.js('resources/assets/js/app.js', 'public/assets/js/')
   .sass('resources/assets/sass/laraspace.scss', 'public/assets/css/')
 
@@ -16,11 +10,3 @@ if (!mix.inProduction()) {
 } else {
   mix.version()
 }
-
-/*
- |--------------------------------------------------------------------------
- | Front
- |--------------------------------------------------------------------------
- */
-
-// mix.sass('resources/assets/front/sass/front.scss', 'public/assets/front/css/').version()
